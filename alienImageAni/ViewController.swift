1ed7486e9d11ff1682387f6d90b87b7925568622
youngju <asdadssad@naver.coim>
2018-04-04 11:58:50 +0900

    change alien image

--- alienImageAni/ViewController.swift
+++ alienImageAni/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienImage: UIImageView!
+    @IBOutlet weak var lblCount: UILabel!
+    var index = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        alienImage.image = UIImage(named: "frame1.png")
+        lblCount.text = String(1)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func btUpdate(_ sender: Any) {
+        
+        if index==5 {
+            index=0
+        }
+        index = index+1
+        
+        var imageFile = "frame\(index).png"
+        alienImage.image = UIImage(named: imageFile)
+        lblCount.text = String(index)
+    
 
-
+    }
 }
 
